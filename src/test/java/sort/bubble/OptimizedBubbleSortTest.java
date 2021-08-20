0328d7ca5383a2299ca0f7c9d848ba41d15b6c07
Mohit Rajvardhan <mohitrajvardhan17@gmail.com>
2021-08-20 09:02:41 +0530

    Fixed indentation issues

--- src/test/java/sort/bubble/OptimizedBubbleSortTest.java
+++ src/test/java/sort/bubble/OptimizedBubbleSortTest.java
@@ -42,7 +42,6 @@ public class OptimizedBubbleSortTest {
         Integer[] result = insertionSort.sort(nums);
         assertNotNull("", result);
         assertEquals("", new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, result);
-
     }
 
     @Test
--- src/test/java/sort/selection/SelectionSortTest.java
+++ src/test/java/sort/selection/SelectionSortTest.java
@@ -41,7 +41,6 @@ public class SelectionSortTest {
         Integer[] result = insertionSort.sort(nums);
         assertNotNull("", result);
         assertEquals("", new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, result);
-
     }
 
     @Test
